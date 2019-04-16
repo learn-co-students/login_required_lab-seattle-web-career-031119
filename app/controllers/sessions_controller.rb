f14b1e06b4e7a55e6a80efe407efd7f4a9a03b00
hoffm386 <erinrhof@uw.edu>
2019-04-16 11:10:08 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def create
+    if params[:name].blank?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
+
+end
